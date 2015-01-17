92fc9dda3ed8997d4342108843e3c82660cb8982
Asya's Air <asya@Anastasias-MacBook-Air.local>
2015-01-17 19:57:53 +0300

    Initial Commit

---
+++ CustomChainOfResponsibility/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CustomChainOfResponsibility
+//
+//  Created by Anastasia Kononova on 1/17/15.
+//  Copyright (c) 2015 Asya Kononova. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CustomChainOfResponsibility/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CustomChainOfResponsibility
+//
+//  Created by Anastasia Kononova on 1/17/15.
+//  Copyright (c) 2015 Asya Kononova. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
